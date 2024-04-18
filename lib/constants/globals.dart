f54484467e4383498b1f9a657a888a32f896b187
Krishnanand Bhat <54655236+Krxshna@users.noreply.github.com>
2024-04-18 14:01:22 +0530

    Add files via upload
    added the constants used in the spatial including onboarding screen and colours needed

---
+++ lib/constants/globals.dart
@@ -0,0 +1,4 @@
+bool isHomeLoaded = false;
+List<double>? locateCoord = [];
+var chooseLocation = true;
+List<int> dist = [5, 50];
---
+++ lib/constants/onboarding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static var primaryColor = const Color.fromARGB(255, 36, 161, 140);
+
+  static var title1 = "Text here 1";
+  static var title2 = "Text here 2";
+  static var title3 = "Text here 3";
+  static var desc1 = "Desc Text here 1";
+  static var desc2 = "Desc Text here 2";
+  static var desc3 = "Desc Text here 3";
+}
